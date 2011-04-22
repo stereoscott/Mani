a80765e51b96cdb63ba6b4c10d7ba8ccbe929a9e
Andrew Noyes <noyesa@gmail.com>
2011-04-21 22:52:37 -0400

    Switched footer nav to wp_nav_menu

--- theme/footer.php
+++ theme/footer.php
@@ -15,13 +15,7 @@
         <div id="footer-wrapper">
             <div class="center">
                 <div id="footer">
-                    <ul id="footer-nav">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Menus</a></li>
-                        <li><a href="#">News &amp; Events</a></li>
-                        <li class="last"><a href="#">Contact Us</a></li>
-                    </ul><!-- #footer-nav -->
+                    <?php wp_nav_menu(array("theme_location" => "primary", "menu_id" => "footer-nav")); ?>
                     <?php get_sidebar( 'footer' ); ?>
                 </div><!-- #footer -->
             </div><!-- .center -->
